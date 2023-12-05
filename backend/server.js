32a4b0d296365e20e94a5f1e0149a98477ce2902
Aldrin Caballero <caballeroaldrin02@gmail.com>
2023-12-05 14:04:03 +0800

    Akin lang si Jenny Rose Perez hmmmm, Iloveyouuuuuuuu

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+require("dotenv").config();
+
+const express = require("express");
+const mysql = require("mysql2");
+
+const PORT = process.env.PORT || 5000;
+const MYSQL_USERNAME = process.env.MYSQL_USERNAME || "root";
+const MYSQL_PASSWORD = process.env.MYSQL_PASSWORD || "";
+const DATABASE_NAME = process.env.DATABASE_NAME || "react_app_eme";
+const app = express();
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: MYSQL_USERNAME,
+  password: MYSQL_PASSWORD,
+  database: DATABASE_NAME,
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL:", err.message);
+  } else {
+    console.log("Connected to MySQL...");
+  }
+});
+
+app.use(express.json());
+app.post("/api/login", (req, res) => {
+  const { username, password } = req.body;
+  connection.query(
+    "SELECT * FROM users WHERE username = ? AND password = ?",
+    [username, password],
+    (err, results) => {
+      if (err) {
+        res
+          .status(500)
+          .json({ error: "Internal Server Error", message: err.message });
+      } else {
+        res.status(201).json(results);
+      }
+    }
+  );
+});
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
