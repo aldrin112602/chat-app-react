271813c4d707d2ec1b52ad084af310d7ecfc465b
Aldrin Caballero <caballeroaldrin02@gmail.com>
2023-12-05 14:36:02 +0800

    'Miss%20youuu%20so%20muchh%20asawa%20ko'

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -4,13 +4,28 @@ import AppLogo from "../AppLogo";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-
 const Login = () => {
   const [inputs, setInputs] = useState({});
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(inputs);
+    try {
+      const response = await fetch("http://localhost:5000/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(inputs),
+      });
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Login successful: ", data);
+      } else {
+        console.error("Login failed");
+      }
+    } catch (error) {
+      console.error("Error during login: ", error.message);
+    }
   };
 
   const handleChange = (event) => {
